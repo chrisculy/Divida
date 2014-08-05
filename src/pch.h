0b7ec39c2d41c26fc00e50367b5a2cc47be7fede
Chris Culy <chris@chrisculy.com>
2014-08-04 22:59:41 -0700

    Add backup implementation of std::make_unique.

--- src/pch.h
+++ src/pch.h
@@ -17,7 +17,21 @@
 
 #include <algorithm>
 #include <iomanip>
+#include <memory>
 #include <string>
 #include <sstream>
+
+#if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 9)
+namespace std
+{
+
+template<typename T, typename ...Args>
+unique_ptr<T> make_unique(Args&& ...args)
+{
+	return unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+}
+#endif
 
 #endif
