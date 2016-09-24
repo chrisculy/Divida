83fefb296dbecc28adafa5a41f89411bc6682ff1
Chris Culy <chris@chrisculy.com>
2016-09-24 11:07:57 -0700

    Fix post-checkout hook.

--- hooks/post-checkout.py
+++ hooks/post-checkout.py
@@ -5,7 +5,7 @@ import sys
 
 print("\nRunning post-checkout hook ...\n")
 
-is_branch_checkout = sys.argv[3] == 1
+is_branch_checkout = sys.argv[3] == '1'
 if not is_branch_checkout:
 	print("Not a branch checkout; exiting.")
 	sys.exit(0)
